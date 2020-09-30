ada5fc96784fe5683b4ba6166213569807171bc4
иван кузьмич <ivankuzmic210@gmail.com>
2020-10-01 00:45:00 +0300

    1 lab

---
+++ Projects/Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        public static void printConsole<T>(T value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            printConsole("Hello World!");
+            printConsole(14);
+        }
+    }
+}
