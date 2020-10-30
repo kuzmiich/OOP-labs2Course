4dc81a40361711f29a0292371831f092a9bee5ba
иван кузьмич <ivankuzmic210@gmail.com>
2020-10-30 23:27:00 +0300

    add common tasks 4 lab

---
+++ Projects/Lab4/Engine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Laba4
+{
+    class Engine
+    {
+        public static string Common1(int year)
+        {
+            int head = 0;
+            const int startHead = 3, UpTwoHundred = 3, CoefUpTreeHundred = 2;
+            const int zeroYear = 0, TwoHundredYear = 200, TreeHundredYear = 300;
+            if (year < TwoHundredYear && year > zeroYear)
+            {
+                head = year * UpTwoHundred + startHead;
+            }
+            else if (year >= TwoHundredYear && year < TreeHundredYear)
+            {
+                head = TwoHundredYear * UpTwoHundred + (year - TwoHundredYear) * CoefUpTreeHundred + startHead;
+            }
+            else if (year >= TreeHundredYear)
+            {
+                head = TwoHundredYear * UpTwoHundred + (TreeHundredYear - TwoHundredYear) * CoefUpTreeHundred + (year - TreeHundredYear) + startHead;
+            }
+            else
+            {
+                return "Error, incorrect data!";
+            }
+            int eyes = head * 2;
+            return $"Count heads = {head}, count eyes = {eyes}";
+        }
+        // The Greatest
+        private static bool isAllValuesEquel(int[] numbers)
+        {
+            Dictionary<int> dict;
+        }
+        public static string Common2(int[] numbers)
+        {
+            if (isAllValuesEquel(numbers))
+            {
+                return "All values are equal.";
+            }
+            int max = numbers[0], min = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                else if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            return $"Max = {max}, Min = {min}";
+        }
+        public static bool Common3(int number)
+        {
+            List<int> listdividers = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19 };
+            foreach (var el in listdividers)
+            {
+                if (number % el == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string Individual1(int number)
+        {
+            return ""; 
+        }
+    }
+}
---
+++ Projects/Lab4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input count of heads: ");
+            int year = 0;
+            try
+            {
+                year = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(Engine.Common1(year));
+
+            //
+            Console.WriteLine("Fill in the array of numbers:");
+            int[] numbers = null;
+            try
+            {
+                numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(Engine.Common2(numbers));
+
+            //
+            Console.WriteLine("Input number: ");
+            int number = 0;
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("The number is a multiple of numbers: 2, 3, 5, 7, 11, 13, 17 и 19 - " + Engine.Common3(number));
+            //
+
+
+        }
+    }
+}
